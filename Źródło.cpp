c167132f261c531ecd7c83e1df20ab95923cb3d8
BasiaMil <115887317+BasiaMil@users.noreply.github.com>
2022-11-13 23:00:22 +0100

    Wymyślenie szkieletu gry

--- Źródło.cpp
+++ Źródło.cpp
@@ -1,11 +1,50 @@
 /*
-Temat projektu:
-Zakres indywidualny:
+Temat projektu:Pi³ka odbijaj¹ca siê od ruchomego pod³o¿a, tytu³:"Z³ap je wszystkie!"
+Zakres indywidualny: Zadaniem gracza jest odbijanie od paletki, poruszaj¹cej siê w osi poziomej u do³u ekranu, pi³ki, któr¹ musi zbijaæ przeszkody,
+by po zniszeniu ich z³apaæ potworki. Z³apanie wszystkich stworów na planszy pozwoli przejœæ na nastêpny poziom. Punkty gracz zdobywa za zbijanie 
+przeszkód i ³apanie stworków oraz za ukoñczenie gry przed limitem czasu. Ujemne punkty otrzymuje siê za upuszczenie pi³ki.
 */
 
 
-int main() {
-
-
+#include <SFML/Graphics.hpp>
+#include<iostream>
+int main()
+{
+	
 	return 0;
 }
+
+/*
+1)Interface:
+   -Opcje:	
+	Nowa gra
+	Wczytaj gre
+	Wyniki
+	Kolekcja(?)
+	Pomoc
+	Wyjœcie
+   -Zanimowane t³o typu gwiazdy koazywane na wyk³adzie
+2)Nowa gra:
+   -Zaczynamy od poziomu pierwszego
+   -Wybieramy trudnoœæ:
+    >szerokoœæ paletki
+	>szybkoœæ pi³ki
+	>czy s¹ spadaj¹ce obiekty
+   -Podajemy nick
+   -dostajemy ID
+3)Wczytaj gre:
+   -Mo¿emy wybraæ poziom od 1 do ostatnio osi¹gniêtego
+   -Wybieramy trudnoœæ:
+	>szerokoœæ paletki
+	>szybkoœæ pi³ki
+	>czy s¹ spadaj¹ce obiekty
+   -Wyœwiatlaj¹ sie nasze nick i ID
+4)WYniki:
+   -wyœwietlane w fomie tablicy:
+    >miejsce
+	>Nick
+	>ID
+	>Punkty
+
+
+*/
