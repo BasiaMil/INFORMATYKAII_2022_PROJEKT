4c25931467fa6c2c05b31b561903d2f3cb3ca305
BasiaMil <115887317+BasiaMil@users.noreply.github.com>
2022-11-16 00:38:58 +0100

    Update Źródło.cpp

--- Źródło.cpp
+++ Źródło.cpp
@@ -5,12 +5,97 @@ by po zniszeniu ich z³apaæ potworki. Z³apanie wszystkich stworów na planszy pozw
 przeszkód i ³apanie stworków oraz za ukoñczenie gry przed limitem czasu. Ujemne punkty otrzymuje siê za upuszczenie pi³ki.
 */
 
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <SFML/Graphics.hpp>
+#include<random>
 #include<iostream>
+
+const int N = 10;
+sf::CircleShape tab[N];
+class gwiazdozbior {
+private:
+	std::random_device rd;
+public:
+	gwiazdozbior(int N);
+	void draw(sf::RenderWindow &window);
+	void move();
+
+};
+
+gwiazdozbior::gwiazdozbior(int N) {
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> distX(1, 750);
+	std::uniform_int_distribution<> distY(1, 550);
+	std::uniform_int_distribution<> distR(10, 50);
+	
+	float x = 0, y = 0, R=0;	
+	
+	
+	
+	
+	for (int i = 0; i < N; i++) {
+		x = distX(gen);
+		y = distY(gen);
+		R = distR(gen);
+
+		tab[i] = sf::CircleShape(R);
+		tab[i].setPosition(sf::Vector2f(x, y));
+		tab[i].setScale(sf::Vector2f(0.03f, 0.03f));
+
+
+	}
+}
+
+void gwiazdozbior::draw(sf::RenderWindow & window) {
+	for (int i = 0; i < N; i++) 
+	{
+		window.draw(tab[i]);
+	}
+}
+
+void gwiazdozbior::move() {
+	std::mt19937 gen(rd());  
+	std::uniform_int_distribution<> distX(1, 750);
+	std::uniform_int_distribution<> distY(1, 550);
+	std::uniform_int_distribution<> distXl(-20, 20);
+	std::uniform_int_distribution<> distYl(-20, 20);
+	for (int i = 0; i < N; i++) 
+	{
+		tab[i].move(sf::Vector2f(distXl(gen), distYl(gen)));
+		sf::Vector2f position = tab[i].getPosition();
+		if (position.x > 750 || position.x < 0 || position.y > 530 || position.y < 0)
+			tab[i].setPosition(distX(gen), distY(gen));
+	}
+}
+
+
 int main()
 {
-	
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Gwiazdozbior");
+	sf::Clock zegar;
+	gwiazdozbior td(10);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		if (zegar.getElapsedTime().asMilliseconds() > 100.0f) {
+			td.move();//nowa pozycja Toad'ow		
+			zegar.restart();
+		}
+
+		window.clear(sf::Color::Blue);
+		td.draw(window);//metoda draw() obiektu klasy toads
+		window.display();
+	}
+
+
+
 	return 0;
 }
 
